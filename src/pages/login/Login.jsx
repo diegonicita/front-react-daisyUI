14258a6e5e79b11cc75f5e2495da9435e1de449f
diegonicita <diegonicita@gmail.com>
2022-11-12 20:46:57 -0300

    Change Logo SVG and Style on Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -65,11 +65,11 @@ export default function Login() {
           <div className="color-stripe top-stripe">
             <div
               className="color-stripe-bar"
-              style={{ backgroundColor: 'rgb(0, 168, 152)' }}
-            ></div>
+              style={{ backgroundColor: 'rgb(255, 192, 14)' }}
+              ></div>
             <div
               className="color-stripe-bar"
-              style={{ backgroundColor: 'rgb(255, 192, 14)' }}
+              style={{ backgroundColor: 'rgb(238, 77, 48)' }}
             ></div>
             <div
               className="color-stripe-bar"
@@ -79,55 +79,76 @@ export default function Login() {
           <div className="web-preview-header-logo"></div>
           <div className="web-preview-header-spacer"></div>
         </div>
-        <section
-          style={{ display: 'flex', flexDirection: 'column', width: '100vw', alignItems: "center" }}
+        <section className="main-section"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100vw',
+            alignItems: 'center'
+          }}
         >
-          <br />        
-          <br />      
-          <br />      
-          <form
-            onSubmit={handleSubmit}
-            style={{ padding: '0.5rem', width: '400px', border: '1px solid black', display: 'flex', flexDirection: 'column', alignItems: "center", gap: "1rem", borderRadius: '10px' }}
-            >
-            <h1>Bienvenido</h1>
-            <label htmlFor="email" style={{display: "flex", gap: "0.5rem", flexDirection: "column"}}>
-              <strong>Correo</strong>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                autoComplete="email"     
-                style={{paddingLeft: "0.3rem"}}           
-              />
-            </label>
-            {errors.email && touched.email && <div>{errors.email}</div>}
-            <label htmlFor="password" style={{display: "flex", gap: "0.5rem", flexDirection:"column"}}>
-              <strong>Contraseña</strong>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                autoComplete="current-password"
-                style={{paddingLeft: "0.3rem"}}
-              />
-            </label>
-            {errors.password && touched.password && (
-              <div className="error">{errors.password}</div>
-            )}
-            <button type="submit">
-              {stateForm.loading ? 'Cargando...' : 'Iniciar Sesión'}
-            </button>
+          <br />
+          <br />
+          <br />
+          <form onSubmit={handleSubmit} className="login-form">
+            <div className="title">Log In</div>
+            <div className="input-container">
+              <label
+                htmlFor="email"
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  flexDirection: 'column'
+                }}
+              >
+                <strong>Correo</strong>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  autoComplete="email"
+                />
+              </label>
+              {errors.email && touched.email && (
+                <div className="error">{errors.email}</div>
+              )}
+            </div>
+            <div className="input-container">
+              <label
+                htmlFor="password"
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  flexDirection: 'column'
+                }}
+              >
+                <strong>Contraseña</strong>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  autoComplete="current-password"
+                />
+              </label>
+              {errors.password && touched.password && (
+                <div className="error">{errors.password}</div>
+              )}
+            </div>
+            <div className="button-container">
+              <input type="submit" value='Enviar'/>              
+            </div>
             {stateForm.error && <p> Credenciales invalidas</p>}
-          <p>
-            ¿Aún no tienes una cuenta?<span> </span>
-            <a href="/register">Crea una cuenta</a>
-          </p>
+            <br />
+            <p>
+              ¿Aún no tienes una cuenta?<span> </span>
+              <a href="/register">Crea una cuenta</a>
+            </p>
           </form>
         </section>
       </div>
