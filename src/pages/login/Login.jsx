7a7873cac92061e67e4bc2fea28f6c4f63bc7886
diegonicita <diegonicita@gmail.com>
2022-11-24 19:13:41 -0300

    merge branch threejs and original-login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useUserStore } from '../../redux/hooks/useUser'
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header'
@@ -7,12 +7,20 @@ import { Canvas, extend } from '@react-three/fiber'
 import { Mark } from '../../components/Mark'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Footer } from '../../components/Footer'
+import * as yup from 'yup';
+import { onSetToken } from '../../redux/slices'
+import axios from 'axios';
+import {useFormik} from 'formik'
 
 extend({ OrbitControls });
-import { onSetToken } from '../../redux/slices'
 
 export default function Login() {
- 
+  const initialValues = {
+    email: '',
+    password: ''
+  }
+
+  const [stateForm, setStateForm] = useState({ loading: false, error: false }) 
   const { user, setUser, setToken } = useUserStore()
 
   const validationSchema = yup.object().shape({
@@ -146,6 +154,79 @@ export default function Login() {
               <a href="/register">Crea una cuenta</a>
             </p>
           </form>
+        </section>
+        <section className="main-section">
+          <div className="product-wrapper">
+            <div className="left-side">
+              <div className="canvas-container">
+                <Canvas
+                  style={{
+                    border: '1px solid rgba(0,0,0,0)',
+                    backgroundColor: '#FFFFF',
+                    borderRadius: '10px'
+                  }}
+                >
+                  <ambientLight intensity="1" color="#444444" />
+                  <spotLight
+                    color="gray"
+                    intensity={1}
+                    position={[3, 3, 3]}
+                    angle={0.7}
+                    penumbra={1}
+                    castShadow
+                  />
+                  <spotLight
+                    color="gray"
+                    intensity={1}
+                    position={[-15, 12, 2]}
+                    angle={0.5}
+                    penumbra={1}
+                    castShadow
+                  />
+                  <spotLight
+                    color="grey"
+                    intensity={1}
+                    position={[5, 12, 0]}
+                    angle={0.5}
+                    penumbra={1}
+                    castShadow
+                  />
+                  <Mark />
+                </Canvas>
+              </div>
+            </div>
+            <div className="right-side">
+              <div className="product-title"> Amy Kilin Brown </div>
+              <div className="product-price"> $240.00 </div>
+              <div className="product-description-1">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
+                repudiandae veritatis neque quibusdam sint sunt nam at tenetur
+                eligendi praesentium reiciendis beatae cum eaque sit magni nisi,
+                deserunt, corrupti repellendus. Ab illum cupiditate nihil minus
+                voluptatem provident quia libero. Natus dolore provident
+              </div>
+              <br />
+              <div className="product-description-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
+                repudiandae veritatis neque quibusdam sint sunt nam at tenetur
+                eligendi praesentium reiciendis beatae cum eaque sit magni nisi,
+                deserunt, corrupti repellendus. Ab illum cupiditate nihil minus
+                voluptatem provident quia libero. Natus dolore provident ad sunt
+                tempore quaerat fugit reiciendis doloribus quae.{' '}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="description-section">
+        <div className="description-section-title">Description</div>
+          <div className="description-section-text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
+            repudiandae veritatis neque quibusdam sint sunt nam at tenetur
+            eligendi praesentium reiciendis beatae cum eaque sit magni nisi,
+            deserunt, corrupti repellendus. Ab illum cupiditate nihil minus
+            voluptatem provident quia libero. Natus dolore provident ad sunt
+            tempore quaerat fugit reiciendis doloribus quae.{' '}
+          </div>
         </section>
         <Footer />
       </div>
